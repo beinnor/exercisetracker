842753e689b5061a27f3982a4e65502a6796d304
Ronnie <ronnie.bjorkelund@gmail.com>
2018-08-29 23:54:05 +0200

    changed to sub documents instead of two seperate documents

--- models/exercise.js
+++ models/exercise.js
@@ -5,9 +5,8 @@ const Schema = mongoose.Schema;
 const ExerciseSchema = new Schema(
   {
     description: {type: String, required: true, minlength: 5, maxlength: 100},
-    userid: {type: Schema.Types.ObjectId, ref: 'User', required: true},    
     duration: {type: Number, required: true},
-    date: {type: Date, required: true}
+    date: {type: Date, required: true, default: Date.now}
   }
 );
 
