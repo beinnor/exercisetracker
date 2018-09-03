cb4fcfae403e73ce35587493efc820fc01c54e37
Ronnie <ronnie.bjorkelund@gmail.com>
2018-09-03 21:54:33 +0200

    fix issue #3

--- app.js
+++ app.js
@@ -42,9 +42,11 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  const errJson = {
+    errorMessage: err.message
+  };
+
+  res.json(errJson);
 });
 
 module.exports = app;
